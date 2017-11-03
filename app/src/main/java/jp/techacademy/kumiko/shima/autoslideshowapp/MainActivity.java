db300cf58a1b9b838449e251158e4e0eee65fff4
shima <shimax.kuma@gmail.com>
2017-11-03 18:00:46 +0900

    Initial commit（途中）

---
+++ app/src/main/java/jp/techacademy/kumiko/shima/autoslideshowapp/MainActivity.java
@@ -0,0 +1,81 @@
+package jp.techacademy.kumiko.shima.autoslideshowapp;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+
+
+    ImageView mImageView;
+    Button mStartButton;
+    Button mNextButton;
+    Button mBackbutton;
+    Timer mSlideCounter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mBackbutton = (Button) findViewById(R.id.back_button);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+
+        //起動した時にパーミッションの許可状態を確認
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                //許可されている
+                getContentsInfo();
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+            }
+            // Android5系以下の場合
+        } else {
+            getContentsInfo();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+        //画像の情報
+        private void getContentsInfo() {
+            ContentResolver resolver = getContentResolver();
+            Cursor cursor = resolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+                    null, // 項目(null = 全項目)
+                    null, // フィルタ条件(null = フィルタなし)
+                    null, // フィルタ用パラメータ
+                    null // ソート (null ソートなし)
+            );
+        }
+}
